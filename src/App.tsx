bf9711970dc9054ce1ea2b3b72cc30b469cc523d
jordanx2 <jordanmooney9@gmail.com>
2024-02-24 14:33:03 +0000

    project restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './app/styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>initial project</p>
+      <p>hello world</p>
     </div>
   );
 }
