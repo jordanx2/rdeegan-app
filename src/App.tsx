0be26a4129f74df8c792eaa6dcfa08c62363096c
jordanx2 <jordanmooney9@gmail.com>
2024-02-25 18:58:46 +0000

    mission statement section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import './app/styles/App.css';
-import Routing from './app/modules/Routing';
-import Navigation from './app/modules/Navigation';
-import Header from './app/modules/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './app/modules/Home';
+import Contact from './app/modules/Contact';
+import Gallery from './app/modules/Gallery';
+import Customers from './app/modules/Customers';
+import Testimonials from './app/modules/Testimonials';
+import Charity from './app/modules/Charity';
+import Services from './app/modules/Services';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Navigation/>
-      <Routing/>
+      <Router>
+        <Routes>
+          <Route path="/" Component={Home}></Route>
+          <Route path="services" Component={Services}></Route>
+          <Route path="gallery" Component={Gallery}></Route>
+          <Route path="customers" Component={Customers}></Route>
+          <Route path="testimonials" Component={Testimonials}></Route>
+          <Route path="charity" Component={Charity}></Route>
+          <Route path="contact" Component={Contact}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/app/modules/Home.tsx
+++ src/app/modules/Home.tsx
@@ -1,5 +1,18 @@
+import '../styles/Home.css';
+
 export default function Home() {
     return (
-        <p>your home</p>
+      <div className='home-main-container'>
+        <div className='home-mission-statement-container'>
+          <h2>Our Mission Statement</h2>
+          <p>We at Ronan Deegan Painting and Decorating Contractors regard every
+          project with the utmost importance. We understand the individual needs
+          of our clients and strive to provide the highest quality painting and decorating service possible in Ireland.</p>
+
+          <p>Quality is our thing. We work hard to make sure our painting and 
+            decorating services are the best you can get in Ireland. Our team 
+            is all about making your space look great and giving you a service that's reliable and friendly.</p>
+        </div>
+      </div> 
     );
 };
